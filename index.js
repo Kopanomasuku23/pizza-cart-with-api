580855071b5c12b4a92c8521bebacfe29f843a4e
unknown <kopanomasuku@gmail.com>
2023-07-06 14:19:42 +0200

    last commit

--- index.js
+++ index.js
@@ -1,28 +1,125 @@
 document.addEventListener('alpine:init', () => {
     Alpine.data('pizzaCartWithAPIWidget', function () {
         return {
+            pepperoniPizza:0,
+            username: '',
             pizzaTypes: [],
+            featuredpizzas: [],
+            cart_id: '',
+            cart: { total: 0 },
+            checkOutMessage: '',
+            makePayment: false,
+            Amount: 0,
+            counter: 0,
+            
+
             init() {
-                // call the api to load data into the screen here
-                const cart_code = '...';
-                const url = `https://pizza-api.projectcodex.net/api/pizzas`;
-
-                axios.get(url).then((result) => {
-                    const pizzas = result.data.pizzas;
-
-                    // this.pizzas is declared on you AlpineJS Widget.
-                    this.pizzas = pizzas;
+                axios
+                  .get('https://pizza-api.projectcodex.net/api/pizzas')
+                  .then((result) => {
+                    this.pizzaTypes = result.data.pizzas
+                  })
+                  .then(() => {
                     
-                    this.pizzaTypes = pizzas;
-                })
+                    return this.createCart();
+                  })
+                  .then((result) => {
+                    this.cart_id = result.data.cart_code;
+                  });
+              },
+        
+            featuredPizzas() {
+                return axios
+                  .get('https://pizza-api.projectcodex.net/api/pizzas/featured')
+              },
+              
+              postfeaturedPizzas() {
+                return axios
+                  .post('https://pizza-api.projectcodex.net/api/pizzas/featured')
+              },
+        
+              createCart() {
+                return axios
+                  .get('https://pizza-api.projectcodex.net/api/pizza-cart/create?username=' + this.username)
+              },
+        
+              showCartContent() {
+                const url = `https://pizza-api.projectcodex.net/api/pizza-cart/${this.cart_id}/get`;
+        
+                axios
+                  .get(url)
+                  .then((result) => {
+                    this.cart = result.data;
+                  });
+              },
+             
+        
+              buyPizza(pizza) {
+                axios
+                  .post('https://pizza-api.projectcodex.net/api/pizza-cart/add', {
+                    cart_code: this.cart_id,
+                    pizza_id: pizza.id
+                  })
+                  .then(() => {
+                    this.counter++
+                    this.message = "Pizza added to the cart"
+                    this.showCartContent();
+                  })
+                  .then(() => {
+        
+                    return this.featuredPizzas();
+        
+                  })
+                  .then(() => {
+                    return this.postfeaturedPizzas();
+                  })
+                  
+              },
+        
+              
+              removePizza(pizza) {
+    
+                axios
+                  .post('https://pizza-api.projectcodex.net/api/pizza-cart/remove',  {
+                    cart_code: this.cart_id,
+                    pizza_id: pizza.id
+                  })
+                  .then(() => {
+                    this.counter--;
+                    this.message = "Pizza removed from the cart"
+                    this.showCartContent();
+                  })
+                  
+        
+              },
+              payment(pizza) {
 
-                axios.post(url, { data: 'here' }).then((result) => {
-                    const pizzas = result.data.pizzas;
-
-                    // this.pizzas is declared on you AlpineJS Widget.
-                    this.pizzas = pizzas;
+            axios
+                .post('https://pizza-api.projectcodex.net/api/pizza-cart/pay', {
+                    cart_code: this.cart_id,
                 })
-            }
+                .then(() => {
+                if (this.Amount >= this.cart.total) {
+                    this.checkOutMessage = 'Enjoy your pizza';
+                    this.change = this.Amount - this.cart.total;
+                    setTimeout(() => {
+                    this.cart.total = 0;
+                    this.cart_count=0;
+                    this.checkOutMessage = '';
+                    }, 5000);
+    
+                } else if (this.Amount < this.cart.total) {
+                    this.checkOutMessage = 'Sorry, That is not enough money!'
+                    setTimeout(() => {
+                        this.checkOutMessage = ''
+                    }, 5000);
+                }
+  
+            })
+  
+        }
+
+
         }
     })
 })
